61c6ad665bb882358681c8002237c7b2b60147e4
Lester Celestial <lesterc@sourcepad.com>
2014-03-21 13:34:19 +0800

    added api and doorkeeper

---
+++ app/api/api.rb
@@ -0,0 +1,9 @@
+class API < Grape::API
+  prefix 'api'
+
+  default_format :json
+  default_error_formatter :json
+
+  version 'v1', :using => :path, :vendor => 'cash_moolah'
+  mount CashMoolah::Ping
+end
