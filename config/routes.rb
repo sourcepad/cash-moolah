a4e238400f21517a86088d8a90ff0be0c03a43c2
Marvin Allan Lu <nattefrost@Marvins-MacBook-Air.local>
2014-03-21 13:23:07 +0800

    database yml problems

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 CashMoolah::Application.routes.draw do
   devise_for :users
   
+  devise_for :users do
+    get "/login" => "devise/sessions#new"
+    delete "/logout" => "devise/sessions#destroy"
+  end
+
   root to: "landing#home"
 end
