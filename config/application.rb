e197d7cfd1a3703317983cb89e05dcd05bf3a3bf
Lester Celestial <lesterc@sourcepad.com>
2014-03-21 13:01:04 +0800

    added configs for the api

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,23 @@ Bundler.require(:default, Rails.env)
 
 module CashMoolah
   class Application < Rails::Application
+    # Auto-load API and its subdirectories
+    config.paths.add 'app/api', glob: '**/*.rb'
+    config.autoload_paths += Dir["#{Rails.root}/app/api/*"]
 
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = 'utf-8'
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
